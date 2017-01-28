e1ed0312a1b7f72396f43f8f1ae64bb0b2edc359
jtradwil <jtradwil@mtu.edu>
2017-01-28 18:30:41 -0500

    Added map capture to motion script and launch file. Made it so movement stops after 10 minutes. Modified motion algorithm

--- lab_three_ws/src/jackal_map/scripts/rand_move.py
+++ lab_three_ws/src/jackal_map/scripts/rand_move.py
@@ -19,47 +19,40 @@ import random
 import sys
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+import time
+import roslaunch
+import os
 
-angular_min = -1 
-linear_min  = -1 
-angular_max =  2 
-linear_max  =  2 
 
+angular_max =  1 
+linear_max  =  1 
+start_time = 0
 
 # define callback for twist
 def Callback(data):
 	global linear_min, linear_max, angular_min, angular_max
-
+	
 	# debug info
-	rospy.loginfo(rospy.get_caller_id())
-	rospy.loginfo('data.scan_time = {}'.format(data.scan_time))
-	rospy.loginfo('data.angle_min = {}'.format(data.angle_min))
-	rospy.loginfo('data.angle_max = {}'.format(data.angle_max))
-	rospy.loginfo('data.angle_increment = {}'.format(data.angle_increment))
-	rospy.loginfo('data.front_range = {}'.format(data.ranges[540]))
+	#rospy.loginfo(rospy.get_caller_id())
+	#rospy.loginfo('data.scan_time = {}'.format(data.scan_time))
+	#rospy.loginfo('data.angle_min = {}'.format(data.angle_min))
+	#rospy.loginfo('data.angle_max = {}'.format(data.angle_max))
+	#rospy.loginfo('data.angle_increment = {}'.format(data.angle_increment))
+	#rospy.loginfo('data.front_range = {}'.format(data.ranges[360]))
 
 	# set thresholds
-	if data.ranges[540] > 1.2 :
-		angular_min = -8 
-		linear_min  = -8 
-		angular_max =  9 
-		linear_max  =  9
-
-	elif data.ranges[540] > 0.75 :
-		angular_min = -5 
-		linear_min  = -5 
-		angular_max =  6 
-		linear_max  =  6 
-
-	else :
-		angular_min = -2 
-		linear_min  = -2 
-		angular_max =  3 
-		linear_max  =  3 
+	if (min(data.ranges[330:390]) > 2):
+		angular_max =  0 
+		linear_max  =  1
+        else :
+		angular_max = 1
+		linear_max  =  0
 
 
 # define setup and run routine
 def setup():
+    global start_time
+    start_time=time.time()
     # get initial parameters
     # user_rate  = rospy.get_param("/teleop_rand/drunken_robot/user_rate")
 
@@ -69,16 +62,16 @@ def setup():
     # subscribe to all
     rospy.Subscriber("/front/scan", LaserScan, Callback)
     # rate = rospy.Rate(user_rate)
-    rate = rospy.Rate(1)
+    rate = rospy.Rate(5)
 
     # publish to cmd_vel of the jackal
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
     # loop
-    while not rospy.is_shutdown():
+    while not time.time()-start_time>600:
 
 		# generate random movement mapping
-		map = [random.randrange(linear_min,linear_max), random.randrange(angular_min,angular_max)]
+		map = [linear_max, angular_max]#random.randrange(linear_min,linear_max), random.randrange(angular_min,angular_max)]
 
 		# push Twist msgs
 		motion = Twist()
@@ -86,8 +79,8 @@ def setup():
 		motion.angular.z = map[1]
 
 		# publish Twist
-		# pub.publish(motion)
-		# pub = rospy.Publisher("/jackal_velocity_controller/cmd_vel", Twist, queue_size=10)
+		pub.publish(motion)
+		pub = rospy.Publisher("/jackal_velocity_controller/cmd_vel", Twist, queue_size=10)
 
 		# rospy.loginfo('linear_min=%d  linear_max=%d  angular_min=%d  angular_max=%d'%(linear_min, linear_max, angular_min, angular_max))
 		# rospy.loginfo("random movement x = {} z = {}".format(motion.linear.x, motion.angular.z))
@@ -95,6 +88,20 @@ def setup():
 
 		rate.sleep()
 
+    #start save map
+    package='map_server'
+    executable ='map_saver'
+    node = roslaunch.core.Node(package, executable, args="-f "+str(os.path.dirname(os.path.realpath(__file__)))+"/myfile")
+    
+	
+    launch = roslaunch.scriptapi.ROSLaunch()
+    launch.start()
+
+    process = launch.launch(node)
+    while process.is_alive():
+        print process.is_alive()
+    #process.stop()
+    rospy.loginfo("-f "+str(os.path.dirname(os.path.realpath(__file__)))+"/myfile")
 
 # standard ros boilerplate
 if __name__ == "__main__":
