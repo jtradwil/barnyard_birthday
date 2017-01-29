0e9d186eddfbf6b7cd8b79644eac1cbb2fd54e08
Derek Chopp <djchopp@mtu.edu>
2017-01-28 19:17:46 -0500

    Added randomization to smart walk based on objects to the left and right

--- lab_three_ws/src/jackal_map/scripts/rand_move.py
+++ lab_three_ws/src/jackal_map/scripts/rand_move.py
@@ -17,44 +17,97 @@
 import rospy
 import random
 import sys
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import LaserScan
 import time
 import roslaunch
 import os
 
-
-angular_max =  1 
-linear_max  =  1 
 start_time = 0
+
+# Global variables for random bounds
+angular_min = -1 
+linear_min  = -1 
+angular_max =  1 
+linear_max  =  1
+
+# Constants for laser averaging
+front_delta = 15
+side_ang = 45
+side_delta = 5
+side_thresh = 2
+
+# Radian to degree function
+def toAng(rad):
+    ang = rad * 180 / 3.14
+    return ang
+    
+# Averaged Sum of scan points function
+def getSum(start, end, data):
+    angSum = float(0.0)
+    index = start
+    while index < end :
+        angSum = angSum + data.ranges[index]
+        index = index + 1
+        
+    angSum = float(angSum) / float(end-start)
+    
+    return angSum
+
 
 # define callback for twist
 def Callback(data):
-	global linear_min, linear_max, angular_min, angular_max
-	
-	# debug info
-	#rospy.loginfo(rospy.get_caller_id())
-	#rospy.loginfo('data.scan_time = {}'.format(data.scan_time))
-	#rospy.loginfo('data.angle_min = {}'.format(data.angle_min))
-	#rospy.loginfo('data.angle_max = {}'.format(data.angle_max))
-	#rospy.loginfo('data.angle_increment = {}'.format(data.angle_increment))
-	#rospy.loginfo('data.front_range = {}'.format(data.ranges[360]))
-
-	# set thresholds
-	if (min(data.ranges[330:390]) > 2):
-		angular_max =  0 
-		linear_max  =  1
+    global linear_min, linear_max, angular_min, angular_max
+    
+    # Calculate front, left, and right angles in the data array
+    zeroAng = int((((abs(data.angle_min) + abs(data.angle_max)) / data.angle_increment) / 2) - 1)
+    leftAng = zeroAng + int(side_ang / toAng(data.angle_increment))
+    rightAng = zeroAng - int(side_ang / toAng(data.angle_increment))
+    sideOffset = int(side_delta / toAng(data.angle_increment))
+    zeroOffset = int(front_delta / toAng(data.angle_increment))
+    
+    # Compute averages for left, right, and front laser scan spans
+    leftAve = getSum(leftAng, leftAng + sideOffset, data)
+    rightAve = getSum(rightAng - sideOffset, rightAng, data)
+    frontAve = getSum(zeroAng - zeroOffset, zeroAng + zeroOffset, data)
+    
+    # Output for monitoring
+    rospy.loginfo('\t%3.4f  -  %3.4f  -  %3.4f',leftAve,frontAve,rightAve)
+        
+    # Set the threshold levels for randomization
+    
+    # Too close in front, turn left and slowly back up  
+    if frontAve < 1 :
+        angular_min = 0
+        angular_max = 0.5 
+        linear_min  = -0.05 
+        linear_max  = 0
+      
+    # All Clear, randomly drive forward with varying turn  
+    elif (frontAve > 4) and (leftAve > side_thresh) and (rightAve > side_thresh) :
+        angular_min = -0.75
+        angular_max = 0.75
+        linear_min  = 0.75 
+        linear_max  = 1.25
+        
+    # Close to a wall on one side, turn to side with most time
+    else :
+        if leftAve > rightAve :
+            angular_min = 0.25
+            angular_max = 0.5
+            linear_min  = 0.25
+            linear_max  = 0.50
         else :
-		angular_max = 1
-		linear_max  =  0
+            angular_min = -0.5
+            angular_max = -0.25
+            linear_min  = 0.25
+            linear_max  = 0.50
 
 
 # define setup and run routine
 def setup():
     global start_time
     start_time=time.time()
-    # get initial parameters
-    # user_rate  = rospy.get_param("/teleop_rand/drunken_robot/user_rate")
 
     # create node for listening to twist messages
     rospy.init_node("jackal_map")
@@ -62,31 +115,43 @@ def setup():
     # subscribe to all
     rospy.Subscriber("/front/scan", LaserScan, Callback)
     # rate = rospy.Rate(user_rate)
-    rate = rospy.Rate(5)
+    rate = rospy.Rate(50)
 
     # publish to cmd_vel of the jackal
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    
+    # Variables for messages and timing
+    count = 0
+    countLimit = random.randrange(25,75)
+    randLin = float(0.0)
+    randAng = float(0.0)
 
     # loop
     while not time.time()-start_time>600:
 
-		# generate random movement mapping
-		map = [linear_max, angular_max]#random.randrange(linear_min,linear_max), random.randrange(angular_min,angular_max)]
+        # generate random movement mapping at random interval
+        if count < countLimit :
+            count = count + 1
+        else :
+            count = 0
+            countLimit = random.randrange(5,25)
+            randLin = random.uniform(linear_min,linear_max)
+            randAng = random.uniform(angular_min,angular_max)
 
-		# push Twist msgs
-		motion = Twist()
-		motion.linear.x  = map[0]
-		motion.angular.z = map[1]
+        # push Twist msgs
+        linear_msg = Vector3(x=randLin, y=float(0.0), z=float(0.0))
+        angular_msg = Vector3(x=float(0.0), y=float(0.0), z=randAng)
+        publish_msg = Twist(linear=linear_msg, angular=angular_msg)
 
 		# publish Twist
-		pub.publish(motion)
-		pub = rospy.Publisher("/jackal_velocity_controller/cmd_vel", Twist, queue_size=10)
+        pub.publish(publish_msg)
+        pub = rospy.Publisher("/jackal_velocity_controller/cmd_vel", Twist, queue_size=10)
 
 		# rospy.loginfo('linear_min=%d  linear_max=%d  angular_min=%d  angular_max=%d'%(linear_min, linear_max, angular_min, angular_max))
 		# rospy.loginfo("random movement x = {} z = {}".format(motion.linear.x, motion.angular.z))
 		# rospy.logdebug("drunk mode x = {} z = {}".format(motion.linear.x, motion.angular.z))
 
-		rate.sleep()
+        rate.sleep()
 
     #start save map
     package='map_server'
